dd2854292fa742da2607027918f57d2cd8d5424a
Benjamin Towse <ben.towse.10@ucl.ac.uk>
2019-02-02 19:19:09 +0000

    Created test for scenario 2
    Changed scenario 1 and 2 checks to only check central cell for simplicity
    
    Scenario 2: Overcrowding
    	Given a game of life
    	When a live cell has more than three neighbours
    	Then this cell dies

--- testing_script.m
+++ testing_script.m
@@ -1,10 +1,10 @@
-clear
-
 %% Test scenario 0
 % 
 % Given a game of life
 % When there are no live cells
 % Then on the next step there are still no live cells
+
+clear
 
 % seedArray with no live cells
 seedArray = zeros(10,10);
@@ -27,6 +27,8 @@ end
 % When a live cell has fewer than two neighbours
 % Then this cell dies
 
+clear
+
 % test some examples of this case:
 
 % live cell has no neighbours
@@ -44,14 +46,12 @@ seedArray{4} =  [   0     0     0;
                     0     1     0;
                     0     0     1];
                 
-% required result in all cases - no live cells
-reqArray = zeros(3,3);
-
-% run the engine and check each output
-success_tally = zeros(length(seedArray),1);
+% required result in all cases - central cell dies
+% run the engine and check each output - only need to check central cell
+success_tally = zeros(1, length(seedArray));
 for t = 1:length(seedArray)
     outArray = gameOfLife(seedArray{t});
-    if isequal(outArray,reqArray)
+    if outArray(3,3) == 0
         success_tally(t) = 1;
     end
 end
@@ -61,5 +61,50 @@ if all(success_tally)
     display('Passed test scenario 1');
 else
     display(['FAILED test scenario 1. Failed examples ' num2str(find(~success_tally)) '.']);
+end
+
+%% Test scenario 2: Overcrowding
+% Given a game of life
+% When a live cell has more than three neighbours
+% Then this cell dies
+
+clear
+
+% some different overcrowding distributions around central cell in array
+seedArray{1} =  [   0   0   0   0   0;
+                    0   1   1   1   0;
+                    0   0   1   1   0;
+                    0   0   0   0   0;
+                    0   0   0   0   0   ];
+seedArray{2} =  [   0   0   0   0   0;
+                    0   1   1   0   0;
+                    0   0   1   1   0;
+                    0   0   1   0   0;
+                    0   0   0   0   0];
+seedArray{3} =  [   0   0   0   0   0;
+                    0   1   1   1   0;
+                    0   0   1   1   0;
+                    0   1   1   1   0;
+                    0   0   0   0   0   ];
+seedArray{4} =  [   0   0   0   0   0;
+                    0   1   0   1   0;
+                    0   0   1   0   0;
+                    0   0   1   1   0;
+                    0   0   0   0   0   ];
+
+% run the engine and check each output - only need to check central cell
+success_tally = zeros(1, length(seedArray));
+for t = 1:length(seedArray)
+    outArray = gameOfLife(seedArray{t});
+    if outArray(3,3) == 0
+        success_tally(t) = 1;
+    end
+end
+
+% Report result. If any examples are failed, return which ones.
+if all(success_tally)
+    display('Passed test scenario 2');
+else
+    display(['FAILED test scenario 2. Failed examples ' num2str(find(~success_tally)) '.']);
 end
 
