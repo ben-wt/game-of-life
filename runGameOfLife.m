f454c5d613a2d4dca1b442abaa0b90f51ee7fdb7
Benjamin Towse <ben.towse.10@ucl.ac.uk>
2019-02-05 17:38:16 +0000

    Added halt options to the display
    "End now" button terminates the game early and outputs the state of the array at that point.
    
    Closing the display figure causes the game to run directly to the pre-specified end.

--- runGameOfLife.m
+++ runGameOfLife.m
@@ -1,17 +1,21 @@
 function outArray = runGameOfLife(seedArray, numSteps, displayOptions)
 %runGameOfLife: Performs specified number of steps in Game of Life, using
-%specified seed
+%specified seed. Option for visual display, including "End now" button.
 %
 % INPUT
 % seedArray - 2D matrix (0 for dead cells, 1 for live cells)
 % numSteps - positive integer, number of game steps to perform
-% displayOptions - string - 'continuous' displays progress of game
-%       frame-by-frame, any other value gives no display
+% displayOptions - string - 'continuous' displays progress of game frame-
+% by-frame with a 0.5 second pause. Any other value gives no display.
 % OUTPUT
 % outArray - 2D matrix (0 for dead cells, 1 for live cells)
 %
+% "End now" button terminates the game early and outputs the state of the
+% array at that point.
+% Closing the display figure removes the display function and removes the
+% per-generation pause, accelerating the game directly to the end.
+%
 % BT, Feb 2019
-
 
 
 % initialise
@@ -23,9 +27,11 @@ if strcmp(displayOptions, 'continuous')
     % draw grid 
     im = image(currentArray, 'CDataMapping', 'scaled', 'Visible', 'off');
     title('Game of Life')
-    xlabel(['Generation 0'])
+    xlabel('Generation 0')
     formatDisplay(ax, size(currentArray));
     im.Visible = 'on';
+    % create an "End now" button
+    stopButton = uicontrol('Style','toggle','string', 'End now');
 end
     
 %run steps
@@ -33,6 +39,20 @@ for s=1:numSteps
     if strcmp(displayOptions, 'continuous')
         % pause so each step is visible
         pause(0.5)
+        % Check if the display has been closed. If so, proceed to end at 
+        % full speed without display.
+        if ~ishghandle(dispFig)
+            displayOptions = 'none';        
+        else
+            % check if the 'stop' button has been pressed, if so halt and
+            % exit before performing this generation
+            drawnow
+            if stopButton.Value == 1
+                display(['Exited at generation ' num2str(s-1)]);
+                break
+            end
+        end
+
     end
     currentArray = gameOfLife(currentArray);
     if strcmp(displayOptions, 'continuous')
@@ -69,5 +89,3 @@ colmap = [1 1 1; 0 0 1];
 colormap(colmap);
 caxis([0 1])
 end
-
-
